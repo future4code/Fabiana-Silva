28ba0e09515b471f9abb750f2c68ff68e65d59b6
FaBGirl <fabculturabsb@gmail.com>
2021-07-23 01:03:20 -0300

    adição de email

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,64 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO 
+
+//..........1 exercício..........
+// let array
+// console.log('a. ', array)
+
+// array = null
+// console.log('b. ', array)
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
+
+// let i = 0
+// console.log('d. ', array[i])
+
+// array[i+1] = 19
+// console.log('e. ', array)
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+
+// .......... 2 exercício..........
+
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+/*Qual será o valor impresso no console se a entrada do usuário for: 
+"Subi num ônibus em Marrocos" ?*/
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+// ......... 1 exercício ..........
+
+/*Faça um programa que pergunte ao usuário seu nome e seu e-mail. 
+Em seguida, imprima na tela a seguinte mensagem: O e-mail emailDoUsuario foi cadastrado com sucesso. 
+Seja bem-vinda(o), nomeDoUsuario!*/
+
+// .......... 2 exercício ..........
+
+/*Faça um programa que contenha um array com 5 das suas comidas preferidas, armazenado em uma variável.
+ Em seguida, siga os passos:
+
+a) Imprima na tela o array completo
+
+b) Imprima na tela a mensagem "Essas são as minhas comidas preferidas: ", seguida por cada uma das comidas, **uma embaixo da outra**.
+
+c) Aqui vai um desafio: pergunte ao usuário uma comida preferida. Troque a segunda comida da sua lista pela inserida pelo usuário. Imprima na tela a nova lista*/
+
+
+//.......... 3 exercício ..........
+
+// a) Crie um array vazio e guarde-o em uma variável, chamada listaDeTarefas
+// b) Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+// c) Pergunte ao usuário 3 tarefas que ele precise realizar no dia e armazene-as, uma por uma, no array
+// d) Peça ao usuário que digite o índice de uma tarefa que ele já realizou: 0, 1 ou 2
+// e) Remova da lista o item de índice que o usuário escolheu.
+// e) Remova da lista o item de índice que o usuário escolheu.
+
+// .......... DESAFIO ..........
+ 
+// 1. Receba uma frase e retorne um array onde cada elemento é uma das palavras da frase, ignorando os espaços
+/*Dado o array ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"], 
+faça um programa que acha o índice da palavra Abacaxi e imprime tanto o índice quanto o tamanho do array*/
