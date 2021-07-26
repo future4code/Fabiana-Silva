4722da9f1215f8193f4a39a8e84e09e6f4b2bffe
FaBGirl <fabculturabsb@gmail.com>
2021-07-25 21:32:39 -0300

    Projeto quinzenal Revisão Inicial

---
+++ revisaoBanu/revisao/lista_js/exercicios.js
@@ -0,0 +1,130 @@
+// // Exemplos
+
+// // Exercício 0A
+function soma() {
+//   // escreva seu código aqui
+  const num1 = prompt('Digite o primeiro número')
+  const num2 = prompt('Digite o segundo número')
+
+  console.log(Number(num1) + Number(num2))
+// }
+
+// // Exercício 0B
+function imprimeMensagem() {
+  // escreva seu código aqui
+  const mensagem = prompt('Digite sua mensagem')
+
+  console.log(mensagem)
+}
+
+// ---------------------------------------------------
+// Exercícios
+
+// Exercício 1
+function calculaAreaRetangulo() {
+// escreva seu código aqui
+const altura = Number(promp("Digite a altura do retângulo"))
+const largura = Number(prompt("Digite a largura do retângulo"))
+
+  console.log(altura * largura)
+}
+
+// // Exercício 2
+function imprimeIdade() {
+//   // escreva seu código aqui
+const anoAtual = Number(prompt("Digite o ano atual"))
+const anoNascimento = Number(prompt("Digite o ano de seu nascimento"))
+
+console.log(anoAtual - anoNascimento)
+
+}
+
+// Exercício 3
+function calculaIMC() {
+// escreva seu código aqui
+const peso = Number(prompt("Digite o seu peso em kg"))
+const altura = Number(prompt("Digite a sua altura em centimetros"))
+
+console.log(Peso/(Altura * Altura))
+
+
+}
+
+// // Exercício 4
+function imprimeInformacoesUsuario() {
+//   // escreva seu código aqui
+  
+//   let nomeDaUsuaria = prompt("Qual o seu nome")
+//   let Idade = prompt("Digite sua idade"))
+//   let email = prompt("Qual o e-mail")
+  
+//   console.log(`Meu nome é ${nomeDausuaria}, tenho ${Idade}, anos e o meu email é ${email}.`)
+
+  
+}
+
+// Exercício 5
+function imprimeTresCoresFavoritas() {
+  // escreva seu código aqui
+
+const cor1 = prompt("Digite a sua cor favorita?")
+const cor2 = prompt("Digite mais uma cor da sua preferência")
+const cor3 = prompt("Me diga uma última cor?")
+
+const cores = [ cor1, cor2, cor3 ]
+
+console.log(cores)
+}
+
+// Exercício 6
+function retornaStringEmMaiuscula() {
+  // escreva seu código aqui
+
+const frase = "oi"
+const fraseMaiuscula = frase.toUpperCase()
+console.log(fraseMaiuscula)
+}
+
+
+// Exercício 7
+function calculaIngressosEspetaculo() {
+  // escreva seu código aqui
+  const valorDoEspetaculo = prompt ("Qual o curto do espetáculo:")
+  const valorDoIngresso = prompt ("Qual valor do ingresso?")
+
+  console.log(valorDoEspetaculo / valorDoIngresso)
+
+}
+
+// Exercício 8
+function checaStringsMesmoTamanho() {
+  // escreva seu código aqui
+  const string1 = prompt ("Digite um sentimento")
+  const string2 = prompt ("Digite uma música")
+  console.log(string1.length === string2.length)
+}
+
+// Exercício 9
+function checaIgualdadeDesconsiderandoCase() {
+  // escreva seu código aqui
+const string1 = prompt ("Digite um palavra")
+const string2 = prompt ("Digite outra palavra")
+
+
+}
+  console.log(
+
+// Exercício 10
+function checaRenovacaoRG() {
+  // escreva seu código aqui
+}
+
+// Exercício 11
+function checaAnoBissexto() {
+  // escreva seu código aqui
+}
+
+// Exercício 12
+function checaValidadeInscricaoLabenu() {
+  // escreva seu código aqui
+}
