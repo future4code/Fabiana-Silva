40e9fd50fd4a1d7775ec5dc4f8b567d36492ca15
FaBGirl <fabculturabsb@gmail.com>
2021-08-08 20:27:23 -0300

    Projeto quinzenal BlackJack

---
+++ quinzena2/blackjack/desafio.js
@@ -0,0 +1,40 @@
+/**
+ * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
+ * 
+ * 
+    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+    
+    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
+    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
+ * 
+ * 
+ * 
+ */
+
+//8 - Se as duas cartas iniciais do usuário ou do computador forem dois ases (A), as cartas devem ser sorteadas novamente.
+
+/*9 - Após o sorteio das 2 cartas para cada jogador, as duas primeiras cartas do usuário continuando sendo reveladas. 
+A primeira carta do computador é revelada, a segunda é oculta por enquanto. 
+Você deve perguntar ao usuário se ele deseja comprar mais uma carta. Veja abaixo:*/
+
+/*10 - A cada carta comprada pelo usuário, a pontuação dele será somada com o valor da nova carta. 
+Ele poderá comprar cartas até atingir a pontuação de 21 pontos; ou até decidir parar de comprar. 
+Assim que o usuário chegar no limite de pontuação, a mensagem de fim de jogo deve ser mostrada, indicando quem venceu. 
+Veja abaixo, uma sequência possível de mensagens mostradas ao usuário (usando confirm e alert).*/
+
+
+/* 11 - **Quando** o jogador **parar** de comprar cartas (Ter clicado `cancel` ao receber a pergunta 
+"**Deseja comprar mais uma carta?**") **e** tiver a pontuação **menor** ou igual a 21, inicia-se a rodada do computador. 
+erá comprar cartas até que sua pontuação seja igual ou superior a do usuário. 
+Veja abaixo, uma sequência possível de mensagens mostradas ao usuário.*/
+
+
+/*12 - Se a pontuação do usuário for maior que 21, ele perde o jogo. 
+Se a pontuação do computador for maior que 21 (e a do usuário não for), o usuário ganha o jogo.  
+Se as pontuações do usuário e do computador forem iguais, o jogo termina em empate.*/
+
+
+
+
+
+
